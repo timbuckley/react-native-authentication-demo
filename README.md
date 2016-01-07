20f403148d5365f784789143ae53d868b6e81a4f
Tim Buckley <timothypbuckley@gmail.com>
2016-01-06 20:11:30 -0500

    Replace boilerplade index.android.js with defer to main.js

---
+++ README.md
@@ -0,0 +1,38 @@
+#AuthenticationBlah
+
+Simple weather demo built in React Native, making use of `MapView` and `openweathermap`.
+
+[![https://gyazo.com/79829221e12a828d18aa5c39b2baec17](https://i.gyazo.com/79829221e12a828d18aa5c39b2baec17.gif)](https://gyazo.com/79829221e12a828d18aa5c39b2baec17)
+
+
+
+###TODO
+
+* Organize components into individual files.
+* Use more ES6/ES7 features (intentionally neglected ES6 `class`).
+* Consider ways to improve functionality.
+
+
+##Installation
+
+Basic React Native setup requirements [here](https://facebook.github.io/react-native/docs/getting-started.html) and for Android [here](https://facebook.github.io/react-native/docs/android-setup.html).
+
+Summary:
+
+0. Install Xcode
+
+1. Install Node.js 4.0 or newer. `$ brew install node`
+    a. [NVM](https://github.com/creationix/nvm#installation) is also useful to manage your node version.
+
+2. Install [watchman](https://facebook.github.io/watchman/), which like `gulp watch` detects file changes and rebuilds assets `$ brew install watchman`.
+
+3. Install RN binary`$ npm install -g react-native-cli`
+
+4. Run with `$ react-native start`
+
+
+## iOS Simulator
+
+0. `$ open ios/stopwatch.xcodeproj/` - Opens the iOS sub-project in Xcode.
+
+1. Click the "Play" button in Xcode
--- index.android.js
+++ index.android.js
@@ -1,52 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+// Boilerplate to render main.js in iOS
+const React = require('react-native')
+const { AppRegistry } = React
+const Main = require('./src/main')
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
 
-var authenticationblah = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('authenticationblah', () => authenticationblah);
+AppRegistry.registerComponent('authenticationblah', () => Main)
