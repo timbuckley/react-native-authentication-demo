a89e475a86b70dd6a153dd946c2c03e3917b9df3
Tim Buckley <timothypbuckley@gmail.com>
2016-01-06 20:14:45 -0500

    Create reusable touchable button component

---
+++ src/components/common/button.js
@@ -0,0 +1,38 @@
+const React = require('react-native')
+const {
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} = React
+
+module.exports = React.createClass({
+  render: function() {
+    return(
+      <TouchableHighlight
+       style={styles.button}
+       underlayColor={'gray'}
+       onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>
+          {this.props.text}
+        </Text>
+      </TouchableHighlight>
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    borderColor: 'black',
+    marginTop: 10
+  },
+  buttonText: {
+    flex: 1,
+    alignSelf: 'center',
+    fontSize: 20
+  }
+})
